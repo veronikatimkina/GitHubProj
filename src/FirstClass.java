3d9c4c59e49f16443464d1d8ad027fae670360ce
Veronika.Timkina <KLYBi4c3MnDae>
2020-05-13 10:49:00 +0300

    Branch1 commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("changes from master");
+        System.out.println("changes from Branch1");
     }
 }
