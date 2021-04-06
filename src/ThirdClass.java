a4c928be33783752ddc7035c2385c99fe4ac03b1
Veronika.Timkina <veronika.timkina@jetbrains.com>
2021-04-06 18:04:09 +0300

    changes for merge

--- src/ThirdClass.java
+++ src/ThirdClass.java
@@ -1,5 +1,5 @@
 public class ThirdClass {
     public static void main(String[] args) {
-        System.out.println("clone2 changes");
+        System.out.println("clone2 etwt4g34t34t34t");
     }
 }
